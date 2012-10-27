fa4e337af472f096cffd386cbeab82ed25b1541a
Enda McDonagh <oisin123@eircom.net>
2012-10-27 20:04:12 +0100

    sessions working

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,17 @@ class User < ActiveRecord::Base
   attr_accessible :FirstName, :LastName, :email, :password, :password_confirmation
   has_secure_password
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
   validates :FirstName, :LastName, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
