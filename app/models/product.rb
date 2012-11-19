10536fb6d19931fd059a38804110022fa8cbbe57
Enda McDonagh <oisin123@eircom.net>
2012-11-19 11:13:32 +0000

    prototype version 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :product_url, :user_id, :video, :remote_image_url, :questions_attributes, :comments_attributes
 
+  validates :product_url, :presence=>true
+
   has_many :questions, :dependent => :destroy
   has_many :comments, :dependent => :destroy
   accepts_nested_attributes_for :comments, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
